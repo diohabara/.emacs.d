e55180152ccdf12631759ee9f83a15de41e3210a
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2020-02-18 19:02:01 +0900

    update

--- init.el
+++ init.el
@@ -1,2 +1,17 @@
+;; General
+;;; Files
 (setq delete-quto-save-files t) ;; delete auto-save files
 (setq make-backup-files nil) ;; don't make backup files
+
+;;; Sound
+(setq ring-bell-function 'ignore) ;; stop beeping
+
+;;; Key-config
+(define-key global-map (kbd "C-z") 'undo)
+
+;;; Indent
+(setq-default indent-tabs-mode nil)
+(setq tab-width 2)
+
+;;; Highlight
+(global-hl-line-mode t)

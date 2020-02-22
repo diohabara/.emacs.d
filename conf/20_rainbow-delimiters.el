c3588fb572188ca8a5ac42f88970eb2915f11d37
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2020-02-22 13:57:54 +0900

    new setting for delimiters

--- conf/20_rainbow-delimiters.el
+++ conf/20_rainbow-delimiters.el
@@ -1,4 +1,14 @@
-(rainbow-delimiters-mode 1)
-;; (setq rainbow-delimiters-outermost-only-face-count)
+(require 'rainbow-delimiters)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 
-(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
+;; highligh parentheses
+(require 'cl-lib)
+(require 'color)
+(defun rainbow-delimiters-using-stronger-colors ()
+  (interactive)
+  (cl-loop
+   for index from 1 to rainbow-delimiters-max-face-count
+   do
+   (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+    (cl-callf color-saturate-name (face-foreground face) 30))))
+(add-hook 'emacs-startup-hook 'rainbow-delimiters-using-stronger-colors)

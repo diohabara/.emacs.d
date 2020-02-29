bb60f87600f055a02bb4c26e60125a7557ce55c2
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2020-02-29 09:52:39 +0900

    delete jsts.el & add weblang.el

---
+++ conf/10_weblang.el
@@ -0,0 +1,36 @@
+(custom-set-variables
+ '(web-mode-enable-auto-quoting nil)
+ '(web-mode-enable-current-column-highlight t)
+ '(web-mode-enable-current-element-highlight t)
+ '(web-mode-markup-indent-offset 2)
+ )
+
+(put 'web-mode-markup-indent-offset 'safe-local-variable 'integerp)
+
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'"  . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'"  . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'"   . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'"      . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'"    . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'"       . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'"      . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'"      . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'"    . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\'"      . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tsx\\'"      . web-mode))
+
+(with-eval-after-load 'web-mode (sp-local-pair '(web-mode) "<" ">" :actions :rem))
+
+(flycheck-add-mode 'html-tidy 'web-mode)
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+(add-hook
+ 'web-mode-hook
+ '(lambda ()
+    (cond
+     ((string= web-mode-content-type "html")
+      (when (executable-find "tidy") (flycheck-select-checker 'html-tidy)))
+     ((or (string= web-mode-content-type "javascript") (string= web-mode-content-type "jsx"))
+      (when (executable-find "eslint") (flycheck-select-checker 'javascript-eslint))
+      (prettier-js-mode)))))

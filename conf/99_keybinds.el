74be9626bb9a47fa3c28af78bf14212a090927be
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2020-04-29 00:04:40 +0900

    update keybinds

--- conf/99_keybinds.el
+++ conf/99_keybinds.el
@@ -1,2 +1,7 @@
-(define-key global-map (kbd "C-z") 'undo)
+;;; delete
 (define-key global-map (kbd "C-h") 'delete-backward-char)
+
+;;; undo & redo
+(global-unset-key (kbd "C-z"))
+(global-set-key (kbd "C-z")   'undo-fu-only-undo)
+(global-set-key (kbd "C-S-z") 'undo-fu-only-redo)

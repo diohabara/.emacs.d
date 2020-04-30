1baf240683f3023c65151b7992e4ff74eeb2d050
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2020-04-30 21:22:02 +0900

    udpate keybinds

--- conf/99_keybinds.el
+++ conf/99_keybinds.el
@@ -4,4 +4,11 @@
 ;;; undo & redo
 (global-unset-key (kbd "C-z"))
 (global-set-key (kbd "C-z")   'undo-fu-only-undo)
-(global-set-key (kbd "C-S-z") 'undo-fu-only-redo)
+(global-set-key (kbd "C-r") 'undo-fu-only-redo)
+
+;;; quit Emacs
+(global-unset-key (kbd "C-x C-c"))
+(defalias 'exit 'save-buffers-kill-emacs)
+;; set some command instead
+;; (global-set-key (kbd "C-x C-c") 'some-command)
+

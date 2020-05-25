dcb57b60712414563e78649ed7957aa75feab251
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2020-05-25 14:54:38 +0900

    del unused shell

--- conf/30_shell.el
+++
@@ -1,9 +0,0 @@
-;; start eshell
-(global-set-key (kbd "C-x C-c") 'ansi-term)
-
-;; eshell keybinds
-(add-hook 'eshell-mode-hook
-          (lambda ()
-            (define-key eshell-mode-map (kbd "C-p") 'eshell-previous-input)
-            (define-key eshell-mode-map (kbd "C-n") 'eshell-next-input)
-            ))

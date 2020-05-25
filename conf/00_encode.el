117f285c06fdaa3532cb1eae3dadbda41c587667
Kadoi Takemaru <kadoitakemaru0902@g.ecc.u-tokyo.ac.jp>
2020-05-25 13:44:21 +0900

    japanese env

--- conf/00_encode.el
+++ conf/00_encode.el
@@ -1,2 +1,6 @@
+;;; Japasene Environment
+(set-default 'buffer-file-coding-system 'utf-8)
+(set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
 (set-default 'buffer-file-coding-system 'utf-8)
+
